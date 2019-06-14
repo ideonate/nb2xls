8a56245a2a5deb313f3aeba803c54a56e05cc8e2
Dan Lester <dan@danlester.com>
2019-06-14 11:45:44 +0100

    images and tables working

--- nb2xls/exporter.py
+++ nb2xls/exporter.py
@@ -1,12 +1,22 @@
 import copy
 from io import BytesIO
-import xlsxwriter
+import re
+import base64
 
 from nbconvert.exporters import Exporter
 
-#-----------------------------------------------------------------------------
-# Classes
-#-----------------------------------------------------------------------------
+from bs4 import BeautifulSoup
+from bs4.element import NavigableString, Tag
+import xlsxwriter
+
+try:
+    import cv2 # Prefer Open CV2 but don't put in requirements.txt because it can be difficult to install
+    import numpy as np
+    usecv2 = True
+except ImportError:
+    import png # PyPNG is a pure-Python PNG manipulator
+    usecv2 = False
+
 
 class XLSExporter(Exporter):
     """
@@ -20,7 +30,7 @@ class XLSExporter(Exporter):
 
     def _file_extension_default(self):
         """
-        The new file extension is `.test_ext`
+        The new file extension is `.xlsx`
         """
         return '.xlsx'
 
@@ -71,13 +81,90 @@ class XLSExporter(Exporter):
 
     def _write_code(self, o):
         if o.output_type == 'stream':
-            self._write_textplain('stream : '+o.text)
+            self._write_textplain(o.text)
         if o.output_type in ('display_data', 'execute_result'):
-            self._write_textplain('text plain : '+o.data['text/plain'])
+            if 'text/html' in o.data:
+                self._write_texthtml(o.data['text/html'])
+            elif 'image/png' in o.data:
+                width, height = 0, 0
+                if 'image/png' in o.metadata and set(o.metadata['image/png'].keys()) == {'width', 'height'} :
+                    width, height = o.metadata['image/png']['width'], o.metadata['image/png']['height']
+                self._write_image(o.data['image/png'], width, height)
+            elif 'application/json' in o.data:
+                self._write_textplain(o.data['application/json'])
+            elif 'text/plain' in o.data:
+                self._write_textplain(o.data['text/plain'])
 
     def _write_textplain(self, text):
         lines = text.split("\n")
-        print(lines)
         for l in lines:
             self.worksheet.write(self.row, 1, l)
             self.row += 1
+
+    def _write_texthtml(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        self._write_soup(soup)
+
+    def _write_soup(self, soup):
+        s = ''
+        for child in soup.children:
+
+            if isinstance(child, NavigableString):
+                s += child.string
+
+            elif isinstance(child, Tag):
+
+                if len(s) > 0:
+                    # Write accumulated string first
+                    re.sub('\s+', ' ', s)
+                    s = s.strip()
+                    if len(s) > 0:
+                        self.worksheet.write(self.row, 1, s.strip())
+                        self.row += 1
+                        s = ''
+
+                if child.name in ('div', 'body', 'span', 'p'):
+                    self._write_soup(child)
+
+                elif child.name == 'table':
+                    self._write_htmltable(soup)
+
+    def _write_htmltable(self, soup):
+        for tablerow in soup('tr'):
+            col = 1
+            for child in tablerow.children:
+                if isinstance(child, Tag):
+                    if child.name == 'th' or child.name == 'td':
+                        self.worksheet.write(self.row, col, child.get_text())
+                        col += 1
+            self.row += 1
+
+    def _write_image(self, image, want_width, want_height):
+
+        image = base64.b64decode(image)
+
+        image_data = BytesIO(image)
+
+        if usecv2:
+            nparr = np.fromstring(image, np.uint8)
+            img = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
+            height, width = img.shape[:2]
+        else:
+            img = png.Reader(image_data)
+            (width, height, _, _) = img.asDirect()
+
+        x_scale, y_scale = 1.0, 1.0
+
+        if want_height > 0 and height > 0:
+            y_scale = want_height / height
+
+        if want_width > 0 and width > 0:
+            x_scale = want_width / width
+
+        self.row += 1
+
+        self.worksheet.insert_image(self.row, 1, 'image.png',
+                                    {'image_data': image_data, 'x_scale': x_scale, 'y_scale': y_scale})
+        self.worksheet.set_row(self.row, height*y_scale)
+
+        self.row += 1
